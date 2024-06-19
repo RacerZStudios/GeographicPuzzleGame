d609514d15be8e65566e6ca97048f3ef681e4af8
ZacB <zac.bogner@gmail.com>
2024-06-19 00:50:29 -0700

    Menu and Puzzle Platform progress

---
+++ Assets/Geoggraphic_Select.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Geoggraphic_Select : MonoBehaviour
+{
+    private void LoadGeoRegion1()
+    {
+        SceneManager.LoadScene(1); 
+    }
+}
