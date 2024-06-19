4bed2cafe16db84bb97958d6aab2648ac62f1bf6
ZacB <zac.bogner@gmail.com>
2024-06-19 10:41:19 -0700

    Puzzle sequence 1 implementation

--- Assets/PuzzleSequenceManager.cs
+++ Assets/PuzzleSequenceManager.cs
@@ -2,19 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tutorial_Event : MonoBehaviour
+public class PuzzleSequenceManager : MonoBehaviour
 {
     [SerializeField] private GameObject platformSpawn;
-    [SerializeField] private GameObject envIntroPrefab; 
+    [SerializeField] private GameObject envIntroPrefab;
+    [SerializeField] private GameObject textPrompt1;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.layer == LayerMask.NameToLayer("Push"))
         {
-            print(collision); 
+            print(collision);
             Destroy(collision.gameObject);
             Instantiate(platformSpawn);
             Instantiate(envIntroPrefab);
-            Destroy(gameObject); 
+            Destroy(gameObject);
+            textPrompt1.gameObject.SetActive(true); 
         }
     }
 }
